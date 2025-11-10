87d31497853f370ebce3b653470e7a4a8f9582ba
raachaelkee <rlkee@ucdavis.edu>
2025-11-09 18:19:09 -0800

    Initial Commit

---
+++ Inoxity/InoxityApp.swift
@@ -0,0 +1,17 @@
+//
+//  InoxityApp.swift
+//  Inoxity
+//
+//  Created by Rachael Kee on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct InoxityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InoxityTests/InoxityTests.swift
@@ -0,0 +1,17 @@
+//
+//  InoxityTests.swift
+//  InoxityTests
+//
+//  Created by Rachael Kee on 11/9/25.
+//
+
+import Testing
+@testable import Inoxity
+
+struct InoxityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
